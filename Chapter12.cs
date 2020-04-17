c14323302c743b9576cf935b90dc17c7178286c1
joseph achonu <ayiamco@gmail.com>
2020-04-17 23:26:09 +0100

    Added Chapter12

---
+++ Chapter12.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    class Chapter12
+    {
+        public static string GetSqrt(object number)
+        //Question 7
+        {
+            try
+            {
+               double sqrt=Math.Sqrt(Convert.ToDouble(number));
+                if (Convert.ToString(sqrt) =="NaN")
+                { Console.WriteLine("Invalid Input"); }
+                else
+                { return Convert.ToString(sqrt); }
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine("Invalid Input");
+                Console.WriteLine(fe.StackTrace);
+                
+            }
+            finally
+            {
+                Console.WriteLine("GoodBye!!!");
+                
+            }
+            return "Could not finish";
+
+        }
+
+        public static long ReadNumber(int start,int end)
+            //Question 8
+        {
+            Exception f = new Exception("Input is in Wrong Format!!!");
+            Exception o = new Exception("Number Out Of Start and End Range!!!");
+            Console.Write("Enter Number : ");
+            try
+            {
+                long number=Convert.ToInt64(Console.ReadLine());
+                if (number < start || number >end)
+                {
+                    throw o;
+                }
+                else
+                {
+                    return number;
+                }
+            }
+            catch(FormatException)
+            {
+                throw f;
+            }
+            
+           
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,23 +11,23 @@ namespace Practice
             //------------------------Chapter9.SelectTask Test---------------------------------------------------
             //Chapter9.SelectTask();
 
-            //--------------------------------BookShop Test ------------------------------------------
-            //BookShop book_store = new BookShop("Amazon ", "10 Austin Opara Street P/H","private");
-            //Console.WriteLine("BookStore Status: "+ book_store.describe);
-            //book_store.Addbook("bible", 2000, category: "religion");
-            //book_store.Addbook("the gods", 3000, author: "grand master", category: "religion");
-            //Console.WriteLine("BookStore Status: "+book_store.describe);
+            //---------------------Chapter9.GetMax Test---------------------------------------------
+            //Console.WriteLine(Chapter9.GetMax( 1, 2, 3 ,4, 5));
+            //int[] array = { 1, 7, 3, 4, 5, 5 };
+            //List<int> lst = new List<int>(array);
+            //Console.WriteLine(Convert.ToString(Chapter9.CheckGreater(10, array)));
 
-            //---------------------------------------------------------------------------------
-            //char[,] lab =
-            //    {
-            //    {' ', ' ', ' ', '*', ' ', ' ', ' '},
-            //    {'*', '*', ' ', '*', ' ', '*', ' '},
-            //    {' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            //    {' ', '*', '*', '*', '*', '*', ' '},
-            //    {' ', ' ', ' ', ' ', ' ', ' ', 'e'},
-            //    };
-            //FindPath(0, 0, lab);
+            ////---------------------Chapter9.Sort Test--------------------------------------
+            //long[] unsorted_array = new long[] { 1, 2, 3, 4, 5, 9, 1, 24, 3 };//values could be changed
+            //long[] sorted_array = Chapter9.Sort(unsorted_array);
+            //string sorted_string = string.Join(",", sorted_array.ToArray());
+            //string unsorted_string = string.Join(",", unsorted_array.ToArray());
+            //Console.WriteLine("UnSorted Array: " + unsorted_string);
+            //Console.WriteLine("Sorted Array: " + sorted_string);
+
+            ////---------------------- Chapter11.GetTriangleArea --------------------------------------------
+            //Console.WriteLine(Convert.ToString(Chapter11.GetTriangleArea(3, 4, 5)));
+            //Console.WriteLine(Convert.ToString(Chapter11.GetTriangleArea(side_a:3, side_b:4, angle:90)));
 
             //----------------------------Chapter11.isLeap Test--------------------------------------------
             //Console.Write("Enter your year: ");
@@ -37,33 +37,12 @@ namespace Practice
             //---------------------------------Chapter11.GetDayCount Test --------------------------------
             //Console.WriteLine("Work Day Count: "+ Chapter11.GetDayCount("04/21/2020"));
 
-            //---------------------Chapter9.GetMax Test---------------------------------------------
-            //Console.WriteLine(Chapter9.GetMax( 1, 2, 3 ,4, 5));
-            int[] array = { 1, 7, 3, 4, 5, 5 };
-            List<int> lst = new List<int>(array);
-            Console.WriteLine(Convert.ToString(Chapter9.CheckGreater(10, array)));
-
-            //---------------------Chapter9.Sort Test--------------------------------------
-            long[] unsorted_array = new long[] { 1, 2, 3, 4, 5, 9, 1, 24, 3 };//values could be changed
-            long[] sorted_array = Chapter9.Sort(unsorted_array);
-            string sorted_string = string.Join(",", sorted_array.ToArray());
-            string unsorted_string = string.Join(",", unsorted_array.ToArray());
-            Console.WriteLine("UnSorted Array: " + unsorted_string);
-            Console.WriteLine("Sorted Array: " + sorted_string);
-
-            //---------------------- Chapter11.GetTriangleArea --------------------------------------------
-            Console.WriteLine(Convert.ToString(Chapter11.GetTriangleArea(3, 4, 5)));
-            Console.WriteLine(Convert.ToString(Chapter11.GetTriangleArea(side_a:3, side_b:4, angle:90)));
-
-
-
+            //Chapter12.GetSqrt("boy");
+            //Console.WriteLine(Convert.ToString(null));
+            Chapter12.ReadNumber(1, 100);
 
         }
 
-        
-       
-
-        
         static void FindPath(int row, int col, char[,] lab)
         {
             if (row < 0 || col < 0 || row >= lab.GetLength(0) || col >= lab.GetLength(1))
