223e2314a4d5830b0b738f63527f167d1c74eb59
joseph achonu <ayiamco@gmail.com>
2020-04-19 21:28:52 +0100

    Added Stacks and Queues

---
+++ Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+
+{
+    class Queue : Stack
+    {
+        List<object> storage_drive = new List<object>();
+        public Queue()
+        {
+
+        }
+
+        public object Enqueue(object item)
+        {
+            storage_drive.Insert(0, item);
+            return item;
+        }
+
+        public object Dequeue()
+        {
+            object dequeue_object = storage_drive[storage_drive.Count - 1];
+            storage_drive.RemoveAt(storage_drive.Count - 1);
+            return dequeue_object;
+        }
+        public new virtual string View()
+        {
+            string view_output = String.Join(",", storage_drive);
+            return view_output;
+        }
+
+        public new virtual int Size()
+        {
+            return storage_drive.Count;
+        }
+
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    class Stack
+    {
+        List<object> storage_drive = new List<object>();
+        public Stack()
+        {
+
+        }
+
+        public bool IsEmpty()
+        {
+            if (storage_drive.Count > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Push(object item)
+        {
+            storage_drive.Add(item);
+        }
+
+        public object Peek()
+        {
+
+            if (storage_drive.Count > 0)
+                return storage_drive[storage_drive.Count - 1];
+            else
+                return -1;
+        }
+
+        public object Pop()
+        {
+            object pop_object = storage_drive[storage_drive.Count - 1];
+            int index = storage_drive.Count - 1;
+            storage_drive.RemoveAt(index);
+            return pop_object;
+        }
+        public string View()
+        {
+            string view_output = String.Join(",", storage_drive);
+            return view_output;
+        }
+        public int Size()
+        {
+            return storage_drive.Count;
+        }
+
+
+    }
+}
